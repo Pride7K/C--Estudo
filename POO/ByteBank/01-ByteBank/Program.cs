099812b9f701a06a4fbb3455f1eef782e1d9e75c
Guilherme Santos Figueiredo <52872315+Pride7K@users.noreply.github.com>
2019-12-11 12:37:53 -0300

    Desenvolvido atráves da Alura

---
+++ POO/ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.saldo = 18;
+
+            conta.saldo += 82;
+
+            conta.titular = "Gui";
+
+            Console.WriteLine($"Nome de titular é {conta.titular}");
+
+            Console.WriteLine($"O saldo da conta é de {conta.saldo}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ POO/ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Guilherme";
+            conta.agencia = 863;
+            conta.numero = 863452;
+
+            ContaCorrente conta3 = new ContaCorrente();
+
+            conta3.titular = "Guilherme";
+            conta3.agencia = 863;
+            conta3.numero = 863452;
+
+            ContaCorrente conta2 = new ContaCorrente();
+
+            conta2.titular = "Guilherme";
+            conta2.agencia = 863;
+            conta2.numero = 863452;
+
+
+
+            Console.WriteLine($" Igualde de tipo de referencia {(conta == conta2)}");
+
+            Console.WriteLine(conta3 == conta);
+
+            Console.WriteLine(conta.titular == conta3.titular);
+
+            Console.WriteLine($" Igualde de tipo de referencia {(conta == conta3)}");
+
+            conta = conta3;
+
+            Console.WriteLine($" Igualde de tipo de referencia {(conta == conta3)}");
+
+            conta3.saldo = 300;
+
+            Console.WriteLine(conta.saldo);
+
+            Console.WriteLine(conta3.saldo);
+
+        }
+    }
+}
