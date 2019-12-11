279cb90755c93fd577669e74929d8ce82bb2e3fe
Guilherme Santos Figueiredo <52872315+Pride7K@users.noreply.github.com>
2019-12-11 17:46:09 -0300

    Add files via upload

---
+++ POO/ByteBank/07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,102 @@
+﻿
+
+
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+
+        private double _saldo = 100;
+
+        private int _agencia;
+
+        public static int _TotalContas { get; private set; }
+
+        
+
+        public int Numero { get; set; }
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+                _agencia = value;
+            }
+        }
+
+        public Cliente Titular { get; set; }
+
+        public double Saldo { get; set; }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            _TotalContas++;
+        }
+
+         
+
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+
+        public void SetSaldo(double saldo)
+        {
+            if (saldo < 0)
+            {
+                /* Return sem nada é tipo um break para metodos */
+                return;
+            }
+            else
+            {
+                this._saldo += saldo;
+            }
+        }
+
+        /* função sempre com letra maiuscular na letra inicial */
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor > _saldo)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+
+
+
+    }
+}
---
+++ POO/ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867,273823);
+            Cliente cliente = new Cliente();
+
+
+            ContaCorrente conta2 = new ContaCorrente(867, 273823);
+
+         
+            ContaCorrente conta3 = new ContaCorrente(867, 273823);
+            
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            conta.Agencia = 4444;
+            conta.Numero = 121212;
+
+            
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+            Console.WriteLine(ContaCorrente._TotalContas);
+
+        }
+    }
+}
