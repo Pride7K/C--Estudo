b8d3f4fbfadd75a814a60ef539ee15dfb9b3cd44
Guilherme Santos Figueiredo <52872315+Pride7K@users.noreply.github.com>
2019-12-12 16:57:40 -0300

    Add files via upload

---
+++ Exceptions/07-ByteBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(32323, 23232323);
+
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            //ContaCorrente conta2 = null;
+           // Console.WriteLine(conta2.Saldo);
+        }
+    }
+}
